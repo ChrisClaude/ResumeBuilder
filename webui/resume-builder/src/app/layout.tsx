bf314b881ddd475b119ca5304f67f2ea9c6fa419
Chris Claude <christ.tchambila@gmail.com>
2025-02-17 12:54:39 +0100

    suppressContentEditableWarning

--- webui/resume-builder/src/app/layout.tsx
+++ webui/resume-builder/src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressContentEditableWarning={true}>
       <body>
         <AppClientComponentWrapper>{children}</AppClientComponentWrapper>
       </body>
