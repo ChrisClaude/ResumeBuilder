8d800e1c7332b218c40774f425b3daa320a8433f
Chris Claude <christ.tchambila@gmail.com>
2025-02-17 10:26:16 +0100

    Add login header

---
+++ webui/resume-builder/src/app/components/Header.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+// import { signIn } from 'next-auth/react';
+import { Button } from '@heroui/react';
+
+const Header = () => {
+  // const login = () => {
+  //   signIn('azure-ad-b2c');
+  // };
+
+  return (
+    <div className="flex justify-between items-center">
+      <h1 className="text-2xl font-bold">Resume Builder 123</h1>
+      <Button color="primary">Button</Button>
+    </div>
+  );
+};
+
+export default Header;
--- webui/resume-builder/src/app/components/SessionProviderWrapper.tsx
+++ webui/resume-builder/src/app/components/SessionProviderWrapper.tsx
@@ -1,8 +1,12 @@
+"use client";
 import { SessionProvider } from 'next-auth/react';
 import React from 'react';
+import { HeroUIProvider } from '@heroui/react';
 
 const SessionProviderWrapper = ({children}: {children: React.ReactNode}) => {
-  return <SessionProvider>{children}</SessionProvider>;
+  return   <HeroUIProvider>
+    <SessionProvider>{children}</SessionProvider>
+  </HeroUIProvider>;
 };
 
 export default SessionProviderWrapper;
