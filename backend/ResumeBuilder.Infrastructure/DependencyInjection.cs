4be7c85edc372df2bc0898d22189756726cf0de2
Chris Claude <christ.tchambila@gmail.com>
2025-03-12 20:20:50 +0100

    Update DI container

--- backend/ResumeBuilder.Infrastructure/DependencyInjection.cs
+++ backend/ResumeBuilder.Infrastructure/DependencyInjection.cs
@@ -1,8 +1,20 @@
 using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ResumeBuilder.Infrastructure.Data;
 
 namespace ResumeBuilder.Infrastructure;
 
-public class DependencyInjection
+public static class DependencyInjection
 {
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<RBContext>(options =>
+        {
+            options.UseSqlServer(configuration.GetConnectionString("ResumeBuilderDb"));
+        });
 
+        return services;
+    }
 }
--- backend/ResumeBuilderAPI/DependencyInjection.cs
+++ backend/ResumeBuilderAPI/DependencyInjection.cs
@@ -1,25 +1,77 @@
-using System;
+using ResumeBuilder.Infrastructure;
 using Scalar.AspNetCore;
 
 namespace ResumeBuilderAPI;
 
 public static class DependencyInjection
 {
-	public static WebApplication ConfigureServices(this WebApplicationBuilder builder)
-	{
-		builder.Services.AddOpenApi();
-		return builder.Build();
-	}
-	public static WebApplication ConfigureRequestPipeline(this WebApplication app)
-	{
-		if (app.Environment.IsDevelopment())
-		{
-			app.MapOpenApi();
-			app.MapScalarApiReference();
-		}
+    private static readonly string _corsPolicyName = "AllowCorsPolicy";
 
-		app.UseHttpsRedirection();
+    public static WebApplication ConfigureServices(this WebApplicationBuilder builder)
+    {
+        var services = builder.Services;
+        services.AddOpenApi();
+        services.AddInfrastructure(builder.Configuration);
 
-		return app;
-	}
+        var logger = ConfigureLogging(services);
+
+        ConfigureCors(services, builder.Configuration, logger);
+        return builder.Build();
+    }
+
+    public static WebApplication ConfigureRequestPipeline(this WebApplication app)
+    {
+        if (app.Environment.IsDevelopment())
+        {
+            app.MapOpenApi();
+            app.MapScalarApiReference();
+        }
+
+        app.UseHttpsRedirection();
+
+        return app;
+    }
+
+    private static ILogger ConfigureLogging(IServiceCollection services)
+    {
+        services.AddLogging((loggingBuilder) =>
+            loggingBuilder.SetMinimumLevel(LogLevel.Trace)
+                .AddSimpleConsole(config => config.SingleLine = true));
+
+        using var loggerFactory = LoggerFactory.Create(loggingBuilder =>
+        {
+            loggingBuilder
+                .SetMinimumLevel(LogLevel.Trace)
+                .AddSimpleConsole(config => config.SingleLine = true);
+        });
+
+        var logger = loggerFactory.CreateLogger("Startup");
+
+        logger.LogInformation("Logging has been configured");
+
+        return logger;
+    }
+
+    private static void ConfigureCors(IServiceCollection services, ConfigurationManager configuration, ILogger logger)
+    {
+        var allowedCorsOrigins = configuration.GetSection("AllowedCorsOrigins").Get<string[]>();
+
+        for (var i = 0; i < allowedCorsOrigins?.Length; i++)
+        {
+            logger.LogInformation("{message}", $"Allowed CORS origin {i}: {allowedCorsOrigins[i]}");
+        }
+
+        services.AddCors(options =>
+        {
+            options.AddPolicy(_corsPolicyName,
+                builder =>
+                {
+                    if (allowedCorsOrigins != null)
+                        builder.WithOrigins(allowedCorsOrigins);
+
+                    builder.AllowAnyHeader()
+                           .AllowAnyMethod();
+                });
+        });
+    }
 }
