30ca2c569fe440fd83d921b5423685bf7470c68f
Chris Claude <christ.tchambila@gmail.com>
2025-04-03 16:19:27 +0200

    Update azure b2c UI

--- webui/resume-builder/src/app/layout.tsx
+++ webui/resume-builder/src/app/layout.tsx
@@ -14,6 +14,7 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+// https://montclaire-fluid-demo.squarespace.com/
   return (
     <html lang="en" suppressContentEditableWarning={true}>
       <Script
